d2fbb1c1bc7c063e948b70298806d75ee058a5b1
Alan Dipert <alan@dipert.org>
2012-05-05 17:37:10 -0400

    DRYing things out

--- lib/rns.rb
+++ lib/rns.rb
@@ -3,38 +3,28 @@ module Rns
     base.extend(ClassMethods)
   end
 
-  def self.add_methods(to, from, method_names)
-    method_names.each do |name|
-      to.send(:define_method, name) do |*args|
-        from.method(name).call(*args)
+  def self.add_methods(to, use_spec)
+    use_spec.to_a.each do |from, method_names|
+      method_names.each do |name|
+        to.send(:define_method, name) do |*args|
+          from.method(name).call(*args)
+        end
       end
     end
   end
 
   module ClassMethods
     def include_specified(use_spec)
-      use_spec.to_a.each do |pkg, method_names|
-        Rns::add_methods(self, pkg, method_names)
-      end
+      Rns::add_methods(self, use_spec)
     end
 
     def extend_specified(use_spec)
       singleton_class = class << self; self; end
-      use_spec.to_a.each do |pkg, method_names|
-        Rns::add_methods(singleton_class, pkg, method_names)
-      end
-    end
-  end
-
-  def self.populate(obj, use_spec)
-    obj.tap do |o|
-      use_spec.to_a.each do |pkg, method_names|
-        Rns::add_methods(o.class, pkg, method_names)
-      end
+      Rns::add_methods(singleton_class, use_spec)
     end
   end
 
   def self.using(use_spec, &blk)
-    blk[Rns::populate(Object.new, use_spec)]
+    blk[Object.new.tap{|o| Rns::add_methods(o.class, use_spec)}]
   end
 end
